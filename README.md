817bf60dc2d617ba2ca5ffcaa63409afa963bbc0
Marcos Bueno <mabf007@gmail.com>
2022-12-04 18:44:39 -0300

    Adicionando script a vendas

--- README.md
+++
@@ -1,2 +0,0 @@
-# Oficina de Integração
-Repositório para o projeto da matéria de Oficina de Integração.
--- js/vendas.js
+++ js/vendas.js
@@ -4,26 +4,37 @@ document.querySelector('.btn-add').addEventListener('click', ()=>{
     console.log(codigo);
     let produto = '';
     let preco = 0;
-    if(codigo == 237428){
+    if(codigo == 1){
         produto = 'Cerveja 350ml';
         preco = 3.50;
-    }else if(codigo == 230900){
+    }else if(codigo == 2){
         produto = 'Vodka 1L';
         preco = 32.00;
+    }else if(codigo == 3){
+        produto = 'Refrigerante 350ml';
+        preco = 3.00;
     }
+    let quantidadeInt = parseInt(quantidade)
 
-    if(codigo !=='' && quantidade !== '' && quantidade !== 0){
+    if(codigo !=='' && quantidade !== '' && quantidadeInt > 0){
         addNewtr(quantidade,produto,preco);
+        let valorTotal = document.querySelector('.valor_total');
+        let valorTotalAux = valorTotal.textContent;
+        let precoTotal = valorTotal.textContent;
+        let resultado = soma(quantidade,preco);
+        console.log(precoTotal);
+        //precoTotal = precoTotal.split
+        precoTotal = precoTotal.replace(/([^\d])+/gim, ' ');
+        let precoTotalAux = [];
+        precoTotalAux = precoTotal.split(' ');
+        console.log(precoTotalAux);
+        console.log(resultado);
+        let aux = parseInt(precoTotalAux[1]) + (parseInt(precoTotalAux[2]) / 100);
+        console.log(aux);
+        document.querySelector('.valor_total').textContent = 'R$ '+(resultado+aux).toFixed(2);
     }
     
-    let valorTotal = document.querySelector('.valor_total');
-    let precoTotal = valorTotal.textContent;
-    let resultado = soma(quantidade,preco);
-    console.log(precoTotal);
-    //precoTotal = precoTotal.split
-    precoTotal = precoTotal.replace(/([^\d])+/gim, '');
-    console.log(precoTotal);
-    document.querySelector('.valor_total').textContent = 'R$ '+resultado;
+    
 
 
 });
@@ -35,3 +46,21 @@ function addNewtr(quantidade, produto, preco){
 function soma(quantidade, preco){
     return preco*quantidade;
 }
+
+document.querySelector('.codigo').addEventListener('keyup',()=>{
+    let codigo = document.querySelector('.codigo').value;
+    console.log(codigo);
+    let produto = '';
+    document.querySelector('.nomeProduto').textContent = '';
+    if(codigo == 1){
+        produto = 'Cerveja 350ml';
+    }else if(codigo == 2){
+        produto = 'Vodka 1L';
+    }else if(codigo == 3){
+        produto = 'Refrigerante 350ml';
+    }
+    let nomeProduto = document.querySelector('.nomeProduto');
+    let texto = document.createTextNode(produto);
+    nomeProduto.appendChild(texto);
+    console.log(nomeProduto.textContent)
+});
